26a6036612d443a8d936d7d1bce56f260f3a8570
Muhammad-Junaid-586 <154722812+Muhammad-Junaid-586@users.noreply.github.com>
2024-08-27 16:12:24 +0500

    resume.pdf deleted

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -68,7 +68,7 @@ const Hero = () => {
         </div>
 
         <a
-          href="Muhammad-Junaid-CV.pdf"
+          href="#"
           download
           className="inline-block bg-blue-600 text-white px-6 py-3 rounded-full text-lg font-semibold hover:bg-blue-700 transition"
         >
